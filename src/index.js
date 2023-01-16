27f76aa7457ab2536247167933d12c04c86bd8e6
AlejandraKeber <alejandrakeber@gmail.com>
2023-01-16 12:31:53 -0500

    Part 2 - Updating the state using the setState()

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import TodoContainer from './components/TodoContainer';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <TodoContainer />
   </React.StrictMode>
 );

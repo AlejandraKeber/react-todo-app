5f26ff5d3cb69003591fa026721e7dca4b0f2bd4
AlejandraKeber <alejandrakeber@gmail.com>
2023-01-16 15:57:52 -0500

    Part 3 - Add styles when any of the todos items is completed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,32 @@
 import React from "react"
 import styles from "./TodoItem.module.css"
 
-function TodoItem(props) {
+class TodoItem extends React.Component {
+  render (){
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
+
   return (
     <li className={styles.item}>
       <input
         type="checkbox"
         className={styles.checkbox}
-        checked={props.todo.completed}
-        onChange={() => props.handleChangeProps(props.todo.id)}
+        checked={this.props.todo.completed}
+        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
       />
-      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
+      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
         Delete
       </button>
-      {props.todo.title}
+      <span style={this.props.todo.completed ? completedStyle : null}>
+        {this.props.todo.title}
+      </span>
     </li>
   )
+}
 }
 
 export default TodoItem
